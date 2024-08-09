d39f15541b250b38f9cc4deff616776cea94bd4f
Gabriel Parreiras <gparreiras46@gmail.com>
2024-08-09 15:52:23 -0300

    db connection

--- index.ts
+++ index.ts
@@ -1,19 +1,47 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+const db = mongoose.connection;
 
-app.get("/", (req, res)=>{
-    res.send("Get funcionando!")
-})
+const db_url: any = process.env.DB_URL;
 
-app.post("/insertTemp", (req, res)=>{
+mongoose.connect(
+  "mongodb+srv://gepeto:Alimento@cluster0.kg0uz.mongodb.net/Temperaturas"
+);
 
-    res.send("Post funcionando!")
+const TemperaturaSchema = new mongoose.Schema({
+    temperatura: String
+  });
 
-})
+const Temperatura = mongoose.model("Temperatura", TemperaturaSchema)
 
+app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log("Servidor rodando na porta 3000")
-})
+db.on("error", () => {
+  console.log("Houve um erro");
+});
+db.once("open", () => {
+  console.log("DataBase loaded");
+});
 
+app.get("/", (req, res) => {
+  res.send("Get funcionando!");
+});
+
+app.post("/insertTemp", async (req, res) => {
+  //res.send("Post funcionando!");
+  const temp = req.body.temperatura;
+    const addTemp = await Temperatura.create({
+        temperatura: temp
+    })
+    if(addTemp){
+        res.send("Temperatura inserida com sucesso")
+    }else{
+        res.send("Erro na insercao da temperatura")
+    }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
